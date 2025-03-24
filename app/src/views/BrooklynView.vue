8ee6215b36b9300aa4b3fade8f8280533468c68c
Benjamin <benyee1209@gmail.com>
2025-03-23 22:42:49 -0400

    Revert "aa"
    This reverts commit d3a100a371ff6187c13a0d0d6a313e1a766b6b79.

---
+++ app/src/views/BrooklynView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class =  "bk">
+  <TheBrooklyn />
+  </div>
+</template>
+
+<script setup>
+//import { Bar } from 'vue-chartjs'
+import TheBrooklyn from '@/components/TheBrooklyn.vue';
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheWelcome />
+    <TheDogBite v-for = "data in data" :key = data.uniqueid  />
+  </div>
+</template>
+
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue';
+import TheDogBite from '@/components/DogBite.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
