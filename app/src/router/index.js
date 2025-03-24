42d2838d3cea2e9bf2323e6acb6bd1c99e437770
Benjamin <benyee1209@gmail.com>
2025-03-23 23:17:06 -0400

    Revert "sdfsd"
    This reverts commit fd9f7ea9592cd41bd844b34ccd0a87e891f402aa.

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -10,12 +10,28 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/brooklyn',
+      name: 'brooklyn',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/BrooklynView.vue'),
+    },
+    {
+      path: '/bronx',
+      name: '/bronx',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/BronxView.vue'),
+    },
+    {
+      path: '/queens',
+      name: '/queens',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/QueensView.vue'),
     },
   ],
 })
---
+++ app/src/views/QueensView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <TheQueens />
+    </div>
+</template>
+
+<script setup>
+import TheQueens from '@/components/TheQueens.vue';
+import DogBite from '@/components/DogBite.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
