d1819c5ee2c7afa56fee3dc682c229052169ce00
Benjamin <benyee1209@gmail.com>
2025-03-23 22:44:40 -0400

    Revert "sss"
    This reverts commit 6de0fc1d26c600dc4339e21e9f67b6c2a7cbf85a.

---
+++ app/src/components/DogBite.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class = "info">
+      <h2>{{ data.uniqueid }}</h2>
+      <h2>{{ data.dateofbite }}</h2>
+      <h2>{{ data.borough}}</h2>
+      <h2>{{ data.sprayneutral}}</h2>
+    </div>
+</template>
+
+<script setup>
+
+const URL = "https://data.cityofnewyork.us/resource/rsgh-akpg.json";
+import {ref, onMounted} from 'vue';
+async function fetchData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+   
+    console.log(data)
+  } catch (err) {
+    console.error(err);
+  }
+}
+onMounted(() =>{
+ fetchData(URL);
+});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/views/BronxView.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="about">
+      <TheBronx />
+    </div>
+    
+</template>
+
+<script setup>
+import TheBronx from '@/components/TheBronx.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
